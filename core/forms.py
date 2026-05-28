839385ca32d8359d3e76fc25a8beeb004cbb5380
ekmountain <ekmountain@gmail.com>
2026-05-28 17:22:40 -0400

    feat: Add transaction management features including create, edit, delete, and list functionalities

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Account, Category, Transaction
+from django.db import models
 
 
 class AccountForm(forms.ModelForm):
@@ -14,3 +15,38 @@ class AccountForm(forms.ModelForm):
                 'placeholder': '0.00'
             }),
         }
+    
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name', 'category_type']
+        widgets = {
+            'name': forms.TextInput(attrs={
+                'placeholder': 'e.g. Side Income'
+            }),
+        }
+
+
+class TransactionForm(forms.ModelForm):
+    class Meta:
+        model = Transaction
+        fields = ['account', 'category', 'amount', 'transaction_type', 'date', 'memo']
+        widgets = {
+            'date': forms.DateInput(attrs={
+                'type': 'date'
+            }),
+            'memo': forms.TextInput(attrs={
+                'placeholder': 'Optional note'
+            }),
+            'amount': forms.NumberInput(attrs={
+                'placeholder': '0.00'
+            }),
+        }
+
+    def __init__(self, user=None, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if user:
+            self.fields['account'].queryset = Account.objects.filter(user=user)
+            self.fields['category'].queryset = Category.objects.filter(
+                models.Q(user=user) | models.Q(user=None)
+            )
